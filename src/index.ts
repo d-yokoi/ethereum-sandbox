868e1974d9d6107f66b50a8398470c6fa6a46f21
d-yokoi <daiki.kouu@gmail.com>
2020-07-04 13:33:50 +0900

    add interaction with ganache

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello ts-template');
+import Web3 from 'web3';
+
+const main = async (): Promise<void> => {
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://0.0.0.0:8545'));
+
+  const wei = await web3.eth.getBalance('0xe330775caD9532A5545168730a579F8f5949dE15');
+
+  console.log(`balance: ${wei} wei`);
+
+  console.log(await web3.eth.getBlockNumber());
+  console.log(await web3.eth.getChainId());
+  console.log(await web3.eth.getAccounts());
+  console.log(await web3.eth.getGasPrice());
+}
+
+main().catch(e => console.error(e));
