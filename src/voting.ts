8aeda39205f401d6da7eedd9c859784abbcc823c
d-yokoi <daiki.kouu@gmail.com>
2020-07-04 21:19:57 +0900

    refactor voting

--- src/voting.ts
+++ src/voting.ts
@@ -9,14 +9,14 @@ const main = async (): Promise<void> => {
   const abi = JSON.parse(fs.readFileSync(`${filepath}.abi`).toString());
 
   const web3 = new Web3(new Web3.providers.HttpProvider('http://0.0.0.0:8545'));
+  const accounts = await web3.eth.getAccounts();
   const contract = new web3.eth.Contract(abi);
 
   const candidates = ['Rama', 'Nick', 'Jose'];
   const candidatesHex = candidates.map(web3.utils.asciiToHex);
 
-  const sender = contract.deploy({ data: bytecode, arguments: [candidatesHex] });
-  const sendOptions = { from: '0x3E26F44030d0CB71De1e6b16c2E5c13EFb802565', gas: 1500000, gasPrice: web3.utils.toWei('0.00003', 'ether') };
-  const instance = await sender.send(sendOptions);
+  const sendOptions = { from: accounts[0], gas: 1500000, gasPrice: web3.utils.toWei('0.00003', 'ether') };
+  const instance = await contract.deploy({ data: bytecode, arguments: [candidatesHex] }).send(sendOptions);
   contract.options.address = instance.options.address;
 
   // https://web3js.readthedocs.io/en/v1.2.0/web3-eth-contract.html#id12
