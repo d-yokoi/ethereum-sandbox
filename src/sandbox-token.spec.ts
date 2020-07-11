7209c9f196c1806ae1346025f0eeeec93a90d68c
d-yokoi <daiki.kouu@gmail.com>
2020-07-12 01:01:10 +0900

    test: add unit test for sandbox token

--- src/sandbox-token.spec.ts
+++ src/sandbox-token.spec.ts
@@ -1,4 +1,24 @@
+import { accounts, contract } from '@openzeppelin/test-environment';
+
+const SandboxToken = contract.fromArtifact('SandboxToken');
+
 describe('sandbox-token', () => {
-  it('should pass', () => {
+  const [sender, recipient] = accounts;
+
+  let sandboxToken: any;
+  beforeEach(async () => {
+    sandboxToken = await SandboxToken.new(210000, { from: sender });
+  });
+
+  it('should transfer token', async () => {
+    const beforeBalance = await sandboxToken.balanceOf(recipient);
+
+    expect(beforeBalance.toString()).toBe('0');
+
+    await sandboxToken.transfer(recipient, 1, { from: sender });
+
+    const afterBalance = await sandboxToken.balanceOf(recipient);
+
+    expect(afterBalance.toString()).toBe('1');
   });
 });
